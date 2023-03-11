2fae536eb9615bb2e63c274e0ca95f0b21ff7689
Wondmeneh Fekadu <wendimar778@gmail.com>
2023-03-11 01:59:02 -0800

    search tag ui implemented

--- script.js
+++ script.js
@@ -5,20 +5,60 @@ document.querySelector(".jsFilter").addEventListener("click", function () {
 document.querySelector(".grid").addEventListener("click", function () {
   document.querySelector(".list").classList.remove("active");
   document.querySelector(".grid").classList.add("active");
-  document.querySelector(".products-area-wrapper").classList.add("gridView");
+  document.querySelector(".bets-area-wrapper").classList.add("gridView");
   document
-    .querySelector(".products-area-wrapper")
+    .querySelector(".bets-area-wrapper")
     .classList.remove("tableView");
 });
 
 document.querySelector(".list").addEventListener("click", function () {
   document.querySelector(".list").classList.add("active");
   document.querySelector(".grid").classList.remove("active");
-  document.querySelector(".products-area-wrapper").classList.remove("gridView");
-  document.querySelector(".products-area-wrapper").classList.add("tableView");
+  document.querySelector(".bets-area-wrapper").classList.remove("gridView");
+  document.querySelector(".bets-area-wrapper").classList.add("tableView");
 });
 
 var modeSwitch = document.querySelector('.mode-switch');
 modeSwitch.addEventListener('click', function () {                      document.documentElement.classList.toggle('light');
  modeSwitch.classList.toggle('active');
 });
+// the tags part
+
+const input = document.querySelector("#tag-input")
+const form = document.querySelector("#tagForm")
+const output = document.querySelector(".tags")
+const max = document.querySelector(".max")
+
+function outputTag(){
+  const tag = `
+    <span class="tag">
+    <b>Hello</b>
+    <span class="material-icons-outlined remove-btn"> remove-btn </span>
+    </span>
+  `;
+  output.innerHTML+= tag;
+  input.value = "";
+}
+tagForm.addEventListener("submit", e=>{
+  if(input.value === ""){
+    e.preventDefault();
+  }
+  
+  else{
+    outputTag()
+  }
+  e.preventDefault()
+}
+)
+input.addEventListener('input', e=>{
+  const rmwhitespace = input.value.replace(/\s/g, '');
+  input.value = rmwhitespace.replace(/\s[^a-zA-Z0-9]/g, "")
+
+});
+window.addEventListener('click', e=>{
+  if(e.target.classList.contains('remove-btn')){
+    e.target.parentElement.remove()
+    input.disabled = false;
+    input.placeholder = "Search an agents ...";
+  }
+})
